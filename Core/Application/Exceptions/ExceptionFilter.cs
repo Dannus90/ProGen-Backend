6d21931e247fb1c287d583b6f3943dc1d62bd2a2
Daniel Persson <persson.daniel.1990@gmail.com>
2021-03-26 23:05:06 +0100

    Finish base for exception handling

--- Core/Application/Exceptions/ExceptionFilter.cs
+++ Core/Application/Exceptions/ExceptionFilter.cs
@@ -11,27 +11,32 @@ namespace Core.Application.Exceptions
         public void OnException(ExceptionContext context)
         {
             Exception exception = context.Exception;
-            ExceptionResponse response = null;
-            Console.WriteLine(exception.GetType());
+            ExceptionResponse response;
             switch (exception)
             {
                 case HttpExceptionResponse e:
                     response = HttpResponseHandler.respond(e);
                     break;
+                
+                default:
+                    Console.WriteLine($"[{exception.GetType().FullName}] {exception.Message}");
+                    Console.WriteLine(exception.StackTrace);
+                    Console.WriteLine(exception.InnerException?.StackTrace);
+                    response = DefaultExceptionResponse();
+                    break;
             }
-            
+
             context.ExceptionHandled = true;
             context.Result = response.CreateObjectResult();
         }
 
         private static ExceptionResponse DefaultExceptionResponse()
         {
-            return new ExceptionResponse
-            {
-                Message = "Internal Server Error",
-                StatusCode = StatusCodes.Status500InternalServerError,
-                
-            }
+            return new ExceptionResponse(
+                StatusCodes.Status500InternalServerError,
+                "Unspecified error",
+                ExceptionTypes.DatabaseError
+            );
         }
     }
 }
---
+++ Core/Application/Exceptions/ExceptionTypes.cs
@@ -0,0 +1,9 @@
+namespace Core.Application.Exceptions
+{
+    public class ExceptionTypes
+    {
+        public const string ServerError = "Internal server error";
+        public const string DatabaseError = "Database error";
+        public const string AuthError = "Authentication error";
+    }
+}
