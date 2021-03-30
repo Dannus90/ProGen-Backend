25dd9c2272f38b65a66e8a4f01f723d53df0602a
Daniel Persson <persson.daniel.1990@gmail.com>
2021-03-30 22:24:41 +0200

    Test CI

---
+++ Tests/TestConfigCi.cs
@@ -0,0 +1,12 @@
+namespace Tests
+{
+    public class TestConfigCi
+    {
+        public const string ConnectionString = @"
+                Host=postgres;
+                Port=5432;
+                Username=progen;
+                Password=progen;
+                Database=progenlocal;";
+    }
+}
