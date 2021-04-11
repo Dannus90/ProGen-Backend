60b0d1afc8f2690b203dfa912d378bdc5b2b5947
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-11 14:08:13 +0200

    Add user data table

---
+++ Core/Domain/DbModels/UserData.cs
@@ -0,0 +1,88 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Domain.DbModels
+{
+    [Table("user_data")]
+    public class UserData
+    {
+        [Key]
+        [Column("id", TypeName = "CHAR(36)", Order = 1)]
+        public Guid Id { get; set; }
+
+        public string IdString
+        {
+            get => Id.ToString("N");
+            set => Id = new Guid(value);
+        }
+        
+        [Column("user_id", TypeName = "Char(36)")]
+        public Guid UserId { get; set; }
+
+        public string UserIdString
+        {
+            get => UserId.ToString("N");
+            set => UserId = new Guid(value);
+        }
+
+        [Required]
+        [Phone]
+        [Column("phone_number", TypeName = "CHAR(64)")]
+        public string PhoneNumber { get; set; } = null;
+
+        [Required]
+        [Column("city_sv", TypeName = "CHAR(128)")]
+        public string CitySv { get; set; } = null;
+
+        [Required]
+        [Column("city_en", TypeName = "CHAR(128)")]
+        public string CityEn { get; set; } = null;
+
+        [Required]
+        [Column("country_sv", TypeName = "CHAR(128)")]
+        public string CountrySv { get; set; } = null;
+        
+        [Required]
+        [Column("country_en", TypeName = "CHAR(128)")]
+        public string CountryEn { get; set; } = null;
+        
+        [Required]
+        [Column("country_sv", TypeName = "CHAR(256)")]
+        public string ProfileImage { get; set; } = null;
+
+        [Column("created_at")] public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")] public DateTime UpdatedAt { get; set; }
+
+        /**
+         * Model configurations.
+         *
+         * @Param {ModelBuilder} modelBuilder - Used for entity configurations in database.
+         */
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserData>()
+                .HasIndex(u => u.UserId)
+                .IsUnique();
+            
+            modelBuilder.Entity<UserData>()
+                .HasOne<User>();
+
+            modelBuilder.Entity<UserData>().Property(u => u.CreatedAt)
+                .HasDefaultValueSql("NOW()")
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<UserData>().Property(u => u.UpdatedAt)
+                .HasDefaultValueSql("NOW()")
+                .ValueGeneratedOnAddOrUpdate();
+
+            modelBuilder.Entity<UserData>()
+                .Ignore(rf => rf.IdString);
+
+            modelBuilder.Entity<UserData>()
+                .Ignore(rf => rf.UserIdString);
+        }
+    }
+}
