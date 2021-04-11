8f9ae65c1345666b8f5c97e3e20f3d3ca9e2d984
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-11 14:48:31 +0200

    Add view model and Dto for user data

---
+++ Core/Domain/ViewModels/UserInformationViewModel.cs
@@ -0,0 +1,9 @@
+using Core.Domain.Dtos;
+
+namespace Core.Domain.ViewModels
+{
+    public class UserInformationViewModel
+    {
+        public FullUserInformationDto FullUserInformationDto { get; set; }
+    }
+}
