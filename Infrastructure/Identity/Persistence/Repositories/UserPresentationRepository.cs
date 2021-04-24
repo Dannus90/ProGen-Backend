20943fee8ad6ff265a7748d3f1fec5bb0cb79854
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-25 00:05:54 +0200

    Minor bug fixes

--- Infrastructure/Identity/Persistence/Repositories/UserPresentationRepository.cs
+++ Infrastructure/Identity/Persistence/Repositories/UserPresentationRepository.cs
@@ -2,7 +2,6 @@ using System;
 using System.Data;
 using System.Threading.Tasks;
 using Core.Domain.DbModels;
-using Core.Domain.Models;
 using Dapper;
 using Infrastructure.Identity.Repositories.Interfaces;
 using Npgsql;
@@ -23,34 +22,59 @@ namespace Infrastructure.Identity.Repositories
             const string queryUserPresentation = @"
                     Insert into user_presentation (id, user_id, presentation_sv, presentation_en)
                     VALUES (@Id, @UserId, @PresentationSv, @PresentationEn);
-
-                    SELECT * FROM user_presentation
-                    WHERE user_id = @UserId;
+                ";
+            
+            const string queryRetrieveUserPresentation = @"
+                    SELECT id AS IdString,
+                        user_Id AS UserIdString,
+                        presentation_sv AS PresentationSv,
+                        presentation_en AS PresentationEn,
+                        created_at AS CreatedAt,
+                        updated_at AS UpdatedAt
+                    FROM user_presentation WHERE user_id = @UserId;  
                 ";
             
             var id = Guid.NewGuid();
             
             using var conn = await connectDb(_connectionString);
             
-            return await conn.ExecuteScalarAsync<UserPresentation>(queryUserPresentation, new
+            // Begin transaction.
+            using var transaction = conn.BeginTransaction();
+            
+            await conn.ExecuteScalarAsync(queryUserPresentation, new
             {
                 userPresentation.PresentationSv,
                 userPresentation.PresentationEn,
                 UserId = userId,
                 Id = id
             });
+            
+            var retreivedUserPresentation = await conn.QueryFirstOrDefaultAsync<UserPresentation>
+            (queryRetrieveUserPresentation, new
+            {
+                UserId = userId
+            });
+            
+            transaction.Commit();
+
+            return retreivedUserPresentation;
         }
         
         public async Task<UserPresentation> GetUserPresentation(string userId)
         {
             const string queryUserPresentation = @"
-                    SELECT * FROM user_presentation
-                    WHERE user_id = @UserId;
+                    SELECT id AS IdString,
+                        user_Id AS UserIdString,
+                        presentation_sv AS PresentationSv,
+                        presentation_en AS PresentationEn,
+                        created_at AS CreatedAt,
+                        updated_at AS UpdatedAt
+                    FROM user_presentation WHERE user_id = @UserId;  
                 ";
             
             using var conn = await connectDb(_connectionString);
             
-            return await conn.ExecuteScalarAsync<UserPresentation>(queryUserPresentation, new
+            return await conn.QueryFirstOrDefaultAsync<UserPresentation>(queryUserPresentation, new
             {
                 UserId = userId
             });
