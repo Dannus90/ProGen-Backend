9a5b8b2d9eac69e6784ec0b01d15e7f538227861
Daniel Persson <persson.daniel.1990@gmail.com>
2021-05-05 23:29:22 +0200

    Try to fix pipeline

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,7 +51,7 @@ namespace API
             _cloudinaryConfig.GetSection("CloudName").Value = configuration["CloudinaryConfig:CloudName"];
             
             // Some secrets come from environment and are set on the hosting platform (or locally) for security reasons.
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() | env.IsEnvironment("TestCi"))
             {
                 _connectionString = _proGenConfig.Get<ProGenConfig>().DbConnectionString;
                 _tokenConfig = _configuration.GetSection("TokenConfig");
