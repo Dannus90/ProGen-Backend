7b306786785b9a052af18ff4ddc9d234649b90b0
Daniel Persson <persson.daniel.1990@gmail.com>
2021-05-07 23:38:39 +0200

    Add more unit tests

--- Tests/UnitTests/PasswordHandlerTests.cs
+++ Tests/UnitTests/PasswordHandlerTests.cs
@@ -1,3 +1,6 @@
+using API.helpers;
+using Core.Application.Exceptions;
+using Core.Domain.Models;
 using Infrastructure.Security;
 using NUnit.Framework;
 
@@ -31,6 +34,68 @@ namespace Tests.UnitTests
                 , hashedHashedPassword);
 
             Assert.AreEqual(false, outcome);
+        }
+        
+        [Test]
+        [TestCase("failmail/gmail.com")]
+        [TestCase("failmail@gmail")]
+        [TestCase("failmail.com")]
+        public void ValidateEmailChange_GivesInvalidEmail_ShouldThrowHttpResponseException(string email)
+        {
+            // Arrange
+            var emailChangeModel = new ChangeEmailModel()
+            {
+                Password = "trallala",
+                NewEmail = email
+            };
+            
+            Assert.Throws<HttpExceptionResponse>(delegate
+            {
+                CredentialsValidation.ValidateEmailChange(emailChangeModel);
+            });
+        }
+        
+        [Test]
+        [TestCase("persson.daniel.1990@gmail.com")]
+        [TestCase("daniel@gmail.com")]
+        [TestCase("persson.daniel.1990@gmail.se")]
+        public void ValidateEmailChange_GivesValidEmail_ShouldNotThrowHttpResponseException(string email)
+        {
+            // Arrange
+            var emailChangeModel = new ChangeEmailModel()
+            {
+                Password = "trallala",
+                NewEmail = email
+            };
+            
+            Assert.DoesNotThrow(delegate
+            {
+                CredentialsValidation.ValidateEmailChange(emailChangeModel);
+            });
+        }
+        
+        [Test]
+        [TestCase("f25422345")]
+        [TestCase("23425")]
+        [TestCase("23452345")]
+        public void ValidatePasswordLength_GivesInvalidPassword_ShouldThrowHttpResponseException(string password)
+        {
+            Assert.Throws<HttpExceptionResponse>(delegate
+            {
+                CredentialsValidation.ValidatePasswordLength(password);
+            });
+        }
+        
+        [Test]
+        [TestCase("f25422345234")]
+        [TestCase("23425234234234")]
+        [TestCase("2345234523423422")]
+        public void ValidatePasswordLength_GivesValidPassword_ShouldNotThrowHttpResponseException(string password)
+        {
+            Assert.DoesNotThrow(delegate
+            {
+                CredentialsValidation.ValidatePasswordLength(password);
+            });
         }
     }
 }
