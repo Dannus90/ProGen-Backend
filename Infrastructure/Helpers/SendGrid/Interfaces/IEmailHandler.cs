d26e99578042fce919453c99013cc85538f9fa20
Daniel Persson <persson.daniel.1990@gmail.com>
2021-05-20 20:23:32 +0200

    Minor change

--- Infrastructure/Helpers/SendGrid/Interfaces/IEmailHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace API.helpers.SendGrid.Interfaces
-{
-    public interface IEmailHandler
-    {
-        Task SendResetPasswordEmail(string email, string token);
-    }
-}
